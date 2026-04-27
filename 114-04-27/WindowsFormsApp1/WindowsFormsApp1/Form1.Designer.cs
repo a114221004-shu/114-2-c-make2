52275ad4a348955d9927380a71df03523a52ff3d
a114221004-shu <a114221004@mail.shu.edu.tw>
2026-04-27 18:08:40 +0800

    115-04-27

---
+++ 114-04-27/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.DTPPublishDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(73, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "書籍名稱";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(73, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "作者";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(73, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "出版日期";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(73, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "價格";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtBookName.Location = new System.Drawing.Point(239, 62);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(512, 46);
+            this.txtBookName.TabIndex = 4;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtAuthor.Location = new System.Drawing.Point(239, 130);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(512, 46);
+            this.txtAuthor.TabIndex = 5;
+            // 
+            // txtCost
+            // 
+            this.txtCost.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtCost.Location = new System.Drawing.Point(239, 258);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(512, 46);
+            this.txtCost.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnAdd.Location = new System.Drawing.Point(344, 341);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(141, 62);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "確定";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // DTPPublishDate
+            // 
+            this.DTPPublishDate.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.DTPPublishDate.Location = new System.Drawing.Point(239, 204);
+            this.DTPPublishDate.Name = "DTPPublishDate";
+            this.DTPPublishDate.Size = new System.Drawing.Size(512, 46);
+            this.DTPPublishDate.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DTPPublishDate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "struct結構宣告範例程式";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DateTimePicker DTPPublishDate;
+    }
+}
+
