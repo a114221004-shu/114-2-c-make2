1289f4acd070d7fa1d43afea292a0b462c66e9e5
a114221004-shu <a114221004@mail.shu.edu.tw>
2026-05-04 18:48:55 +0800

    115-05-04

---
+++ 115-05-04/ConsoleApp_0504/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_0504
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.Name = "賈斯柏";
+            s1.Score_Chi = 90;
+            s1.Score_Math = 70;
+            s1.Score_Eng = 80;
+            s1.ShowSum();
+            s1.ShowPass();
+
+            Student s2 = new Student();
+            s2.Name = "愛麗絲";
+            s2.Score_Chi = 60;
+            s2.Score_Math = 70;
+            s2.Score_Eng = 60;
+            s2.ShowSum();
+            s2.ShowPass();
+
+
+            StudentPro p1 = new StudentPro();
+            p1.Name = "賈斯柏";
+            p1.Score_PL = 50;
+            p1.Score_AI = 40;
+            p1.Score_Cloud = 62;
+            p1.ShowSum();
+            p1.ShowPass();
+
+            StudentPro p2 = new StudentPro();
+            p2.Name = "愛麗絲";
+            p2.Score_PL = 80;
+            p2.Score_AI = 90;
+            p2.Score_Cloud = 82;
+            p2.ShowSum();
+            p2.ShowPass();
+
+        }
+    }
+}
---
+++ 115-05-04/WindowsFormsApp_0504/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_0504
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student s1 = new Student();
+            s1.Name = "賈斯柏";
+            s1.Score_Chi = 90;
+            s1.Score_Math = 70;
+            s1.Score_Eng = 80;
+            s1.ShowSum();
+            s1.ShowPass();
+
+            Student s2 = new Student();
+            s2.Name = "愛麗絲";
+            s2.Score_Chi = 60;
+            s2.Score_Math = 70;
+            s2.Score_Eng = 60;
+            s2.ShowSum();
+            s2.ShowPass();
+        }
+    }
+}
