2ce3ad07ffd092966b99f4f42ab97ba449c254bd
a114221004-shu <a114221004@mail.shu.edu.tw>
2026-05-25 18:52:46 +0800

    115-05-25

---
+++ 115-05-25/Inherits/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Inherits.Empolyee;
+
+namespace Inherits
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Empolyee Jennifer = new Empolyee();  // Employee父類別
+            Jennifer.Name = "珍妮佛";
+            Jennifer.Salary = 30000;
+            LblMsg.Text = $"員工姓名：{Jennifer.Name} \n實領薪水：{Jennifer.Salary}";
+            LblMsg.Text += "\n======================\n";
+            Manager Alice = new Manager();      // Manager子類別
+            Alice.Name = "艾莉絲";
+            Alice.Salary = 40000;
+            Alice.Bonus = 20000;               // Manager子類別新增的Bonus屬性
+            LblMsg.Text += Alice.GetTotal();   //Manager子類別新增的GetTotal()方法
+
+        }
+    }
+}
---
+++ 115-05-25/建構式/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 建構式
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student s1 = new Student();  // 無引數的建構式
+            s1.Name = "布魯斯";
+            s1.Score = 88;
+            LblMsg.Text = s1.GetMsg() + "\n\n";
+
+            Student s2 = new Student("珍妮佛");  //傳一個引數的建構式
+            s2.Score = 77;
+            LblMsg.Text += s2.GetMsg() + "\n\n";
+
+            Student s3 = new Student("艾莉絲", 99);  //傳兩個引數的建構式
+            LblMsg.Text += s3.GetMsg();
+
+        }
+    }
+}
